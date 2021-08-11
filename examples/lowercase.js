12548def8c9179dc78b6f02e7585b651f9ab89fe
Francis <15894826+francisfuzz@users.noreply.github.com>
2021-08-11 17:40:07 +0000

    docs: add lowercase example

---
+++ examples/lowercase.js
@@ -0,0 +1,32 @@
+const generate = require('../index')
+
+const lowercase = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+]
+
+console.log(generate(64, lowercase))
