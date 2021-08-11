a0579cac8c8baf76f407acd0eab7dea2cf931b97
Francis <15894826+francisfuzz@users.noreply.github.com>
2021-08-11 17:42:39 +0000

    refactor: use string+split to generate charset

--- examples/lowercase.js
+++ examples/lowercase.js
@@ -1,32 +1,4 @@
 const generate = require('../index')
-
-const lowercase = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-]
+const lowercase = 'abcdefghijklmnopqrstuvwxyz'.split('')
 
 console.log(generate(64, lowercase))
